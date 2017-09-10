f5720eda595d55c5db8f840d87dcd85df2dd4e97
tarukosu <u.au.sky.future12@gmail.com>
2017-09-10 15:36:36 +0900

    add server program

---
+++ README.md
@@ -0,0 +1,23 @@
+# My Environments
+- Raspberry Pi 3
+- Roomba 622
+
+
+# Dependencies
+```
+$ apt install python-dev
+$ pip install pyserial
+$ pip install mprpc
+```
+
+# setup
+```
+$ git submodule init
+$ git submodule update
+$ touch roomba/__init__.py
+```
+
+# start server
+```
+$ python server.py
+```
---
+++ server.py
@@ -0,0 +1,36 @@
+import roomba.create as create
+import time
+import zerorpc
+
+class RoombaRPC(object):
+    def __init__(self, port):
+        self.robot = create.Create(port)
+        self.robot.toSafeMode()
+        self.moving = False
+        self.pause()
+
+
+    def __del__(self):
+        self.robot.close()
+
+    def toSafeMode(self):
+        self.robot.toSafeMode()
+
+    def pause(self):
+        if self.moving:
+            self.move(0, 0)
+            self.moving = False
+        
+    def move(self, x, theta):
+        print("move: %f, %f" % (x, theta))
+        self.robot.go(x, theta)
+        self.moving = True
+                
+    
+if __name__=='__main__':
+    RPC_PORT = 6000
+    ROOMBA_PORT = "/dev/ttyUSB0"
+    s = zerorpc.Server(RoombaRPC(ROOMBA_PORT))
+    s.bind("tcp://0.0.0.0:{0}".format(RPC_PORT))
+    s.run()
+
